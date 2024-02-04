a40dd3bf06b841bbd4f8f068027090b91289b11b
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-04 16:58:21 +0100

    Pass test of portfolio with same stock returns one position and test case of portfolio with two different positions

--- src/test/java/io/github/bartlomiejmilosz/tdd/portfolio/finalimplementation/PortfolioPositionTest.java
+++ src/test/java/io/github/bartlomiejmilosz/tdd/portfolio/finalimplementation/PortfolioPositionTest.java
@@ -24,4 +24,57 @@ public class PortfolioPositionTest {
         Assertions.assertEquals(260, portfolio.getPositions(symbol).getPx());
         Assertions.assertEquals(2600, portfolio.getPositions(symbol).getValue());
     }
+
+    @Test
+    void portfolioWithTwoDifferentPositions_ReturnsThosePositions() {
+        var portfolio = new Portfolio();
+
+        String microsoft = "MSFT";
+        String apple = "AAPL";
+
+        portfolio.add(position(microsoft, 10, 260));
+        portfolio.add(position(apple, 2, 150));
+
+        Assertions.assertEquals(2, portfolio.getAllPositions().size());
+
+        // msft
+        var microsoftPosition = portfolio.getPositions(microsoft);
+        Assertions.assertEquals(10, microsoftPosition.getQty());
+        Assertions.assertEquals(260, microsoftPosition.getPx());
+        Assertions.assertEquals(2600, microsoftPosition.getValue());
+
+        // aapl
+        var applePosition = portfolio.getPositions(apple);
+        Assertions.assertEquals(2, applePosition.getQty());
+        Assertions.assertEquals(150, applePosition.getPx());
+        Assertions.assertEquals(300, applePosition.getValue());
+    }
+
+    static Position position(String symbol, int qty, double px) {
+        return new Position(new Stock(symbol), qty, px);
+    }
+
+    @Test
+    void portfolioWithSameStock_ReturnsOnePosition() {
+        var portfolio = new Portfolio();
+
+        String microsoft = "MSFT";
+
+        portfolio.add(position(microsoft, 10, 260));
+        portfolio.add(position(microsoft, 5, 200));
+
+        Assertions.assertEquals(1, portfolio.getAllPositions().size());
+    }
+
+    @Test
+    void portfolioWithSameStock_ReturnsCorrectQty() {
+        var portfolio = new Portfolio();
+
+        String microsoft = "MSFT";
+
+        portfolio.add(position(microsoft, 10, 260));
+        portfolio.add(position(microsoft, 1, 200));
+
+        Assertions.assertEquals(11, portfolio.getPositions(microsoft).getQty());
+    }
 }
